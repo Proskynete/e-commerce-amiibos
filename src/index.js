7a2259aa03183214ff8f867f873f1c74a48aa890
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-09 12:22:05 -0400

    Install react-redux redux and thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './views';
 
+import store from './store';
 import './assets/scss/styles.scss';
 
 import * as serviceWorker from './serviceWorker';
 
-const Main = () => <App />;
-
+const Main = () => (
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
 const root = document.getElementById('root');
 render(<Main />, root);
 serviceWorker.unregister();
