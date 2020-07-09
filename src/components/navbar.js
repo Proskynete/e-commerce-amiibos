9ddb6ed4dbd2c08105d0f4517f255e2254996574
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-09 12:07:06 -0400

    Add lint and prettier files for format code

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,57 +10,56 @@ const Navbar = () => {
 
 	return (
 		<nav
-			className="navbar is-fixed-top"
-			role="navigation"
-			aria-label="main navigation"
+			className='navbar is-fixed-top'
+			role='navigation'
+			aria-label='main navigation'
 		>
-			<div className="navbar-brand">
-				<a href="/" className="navbar-item">
-					<img src={LOGO} width="112" height="28" alt="Amiibos logo" />
+			<div className='navbar-brand'>
+				<a href='/' className='navbar-item'>
+					<img src={LOGO} width='112' height='28' alt='Amiibos logo' />
 				</a>
 
 				<div
-					role="menuitem"
-					className="navbar-burger burger"
-					aria-label="menu"
-					aria-expanded="false"
-					data-target="navigation"
+					role='menuitem'
+					className='navbar-burger burger'
+					aria-label='menu'
+					data-target='navigation'
 					onClick={handleSetMobileMenu}
 				>
-					<span aria-hidden="true" />
-					<span aria-hidden="true" />
-					<span aria-hidden="true" />
+					<span aria-hidden='true' />
+					<span aria-hidden='true' />
+					<span aria-hidden='true' />
 				</div>
 			</div>
 
 			<div
-				id="navigation"
+				id='navigation'
 				className={`${showMenu ? 'navbar-menu is-active' : 'navbar-menu'}`}
 			>
-				<div className="navbar-start">
-					<a href="#!" className="navbar-item">
+				<div className='navbar-start'>
+					<a href='#!' className='navbar-item'>
 						Inicio
 					</a>
 
-					<a href="#!" className="navbar-item">
+					<a href='#!' className='navbar-item'>
 						Documentation
 					</a>
 				</div>
 
-				<div className="navbar-end">
-					<div className="navbar-item">
-						<div className="buttons">
-							<a href="#!" className="is-primary">
-								<span className="icon">
-									<span className="fa-stack">
-										<i className="fas fa-user" />
+				<div className='navbar-end'>
+					<div className='navbar-item'>
+						<div className='buttons'>
+							<a href='#!' className='is-primary'>
+								<span className='icon'>
+									<span className='fa-stack'>
+										<i className='fas fa-user' />
 									</span>
 								</span>
 							</a>
-							<a href="#!" className="is-light">
-								<span className="icon">
-									<span className="fa-stack">
-										<i className="fas fa-shopping-cart" />
+							<a href='#!' className='is-light'>
+								<span className='icon'>
+									<span className='fa-stack'>
+										<i className='fas fa-shopping-cart' />
 									</span>
 								</span>
 							</a>
