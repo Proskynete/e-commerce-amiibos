e2dad1ca94d549903215c2a6756a46032876581c
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-09 13:20:25 -0400

    Create routes and create 404 view

--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { routes } from '../routes';
 
-const App = () => <h1>App</h1>;
+const FourZeroFour = lazy(() => import('./four_zero_four'));
+
+const mainRoutes = routes.map((route) => (
+	<Route
+		key={route.name}
+		path={route.path}
+		exact={route.exact}
+		component={(props) => <route.component {...props} />}
+	/>
+));
+
+const App = () => (
+	<Router basename='/'>
+		<Suspense fallback={''}>
+			<Switch>
+				{mainRoutes}
+				<Route path='*' component={(props) => <FourZeroFour {...props} />} />
+			</Switch>
+		</Suspense>
+	</Router>
+);
 
 export default App;
